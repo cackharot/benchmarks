b0e8877c8de363f21009de95e97f4d522339820a
cackharot <cackharot@gmail.com>
2017-11-18 17:43:11 -0500

    update ci config

--- elixir_phoenix_bench/scripts/build.sh
+++ elixir_phoenix_bench/scripts/build.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
+uname -a
+
+TARGET_DIR=$(pwd)/rel-artifacts
+VERSION="0.0.1"
+REL_DIR="_build/dev/rel/phx_bench_api/releases/$VERSION"
+OUTPUT_ARTIFACT="$REL_DIR/phx_bench_api.tar.gz"
+
+cd "$*"
+echo 'Fetch deps, compile, test & release build...'
 mix do deps.get, deps.compile, release --env=prod --verbose
+cp Dockerfile $TARGET_DIR
+cp "_build/dev/rel/phx_bench_api/releases/$VERSION/phx_bench_api.tar.gz" $TARGET_DIR
+ls $TARGET_DIR
+echo 'Success!'
