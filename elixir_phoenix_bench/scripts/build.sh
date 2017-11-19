da4d8a42adcd5ebaf881b8c1160e5cea76f1c439
cackharot <cackharot@gmail.com>
2017-11-19 18:29:43 -0500

    test ci build rc

--- elixir_phoenix_bench/scripts/build.sh
+++ elixir_phoenix_bench/scripts/build.sh
@@ -3,7 +3,7 @@ set -e -x
 
 uname -a
 
-TARGET_DIR=$(pwd)/rel-artifacts
+TARGET_DIR=$(pwd)/build-artifact
 VERSION="0.0.1"
 REL_DIR="_build/dev/rel/phx_bench_api/releases/$VERSION"
 OUTPUT_ARTIFACT="$REL_DIR/phx_bench_api.tar.gz"
@@ -15,5 +15,6 @@ echo 'Fetch deps, compile, test & release build...'
 mix do deps.get, deps.compile, release --env=prod --verbose
 cp Dockerfile $TARGET_DIR
 cp "_build/dev/rel/phx_bench_api/releases/$VERSION/phx_bench_api.tar.gz" $TARGET_DIR
+echo $TARGET_DIR
 ls $TARGET_DIR
 echo 'Success!'
