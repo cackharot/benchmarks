6535b5726af65166f66ce799ddd9ec13eb2183e3
cackharot <cackharot@gmail.com>
2017-11-19 19:23:41 -0500

    update ci

--- elixir_phoenix_bench/scripts/build.sh
+++ elixir_phoenix_bench/scripts/build.sh
@@ -9,13 +9,14 @@ export MIX_ENV=prod
 TARGET_DIR=$(pwd)/build-artifact
 VERSION=$(cat version/number)
 REL_DIR="_build/$MIX_ENV/rel/phx_bench_api/releases/$VERSION"
-OUTPUT_ARTIFACT="$REL_DIR/phx_bench_api.tar.gz"
+SRC_ARTIFACT="$REL_DIR/phx_bench_api.tar.gz"
+OUTPUT_ARTIFACT="$TARGET_DIR/phx_bench_api-$VERSION.tar.gz"
 
 
 cd "$*"
 echo 'Fetch deps, compile & prod release build...'
 mix do deps.get, deps.compile, release
 cp Dockerfile $TARGET_DIR
-cp $OUTPUT_ARTIFACT $TARGET_DIR
+cp $SRC_ARTIFACT $OUTPUT_ARTIFACT
 ls $TARGET_DIR
 echo 'Success!'
