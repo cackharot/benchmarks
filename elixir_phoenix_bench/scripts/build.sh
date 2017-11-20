4daaf1333b1dea169d36c15e53bc903bd93d7b2f
cackharot <cackharot@gmail.com>
2017-11-19 19:02:31 -0500

    update build script

--- elixir_phoenix_bench/scripts/build.sh
+++ elixir_phoenix_bench/scripts/build.sh
@@ -5,18 +5,20 @@ uname -a
 ls
 ls version/*
 cat version/number
+cat version/version
+
+export MIX_ENV=prod
+
 TARGET_DIR=$(pwd)/build-artifact
-VERSION="0.0.1"
-REL_DIR="_build/dev/rel/phx_bench_api/releases/$VERSION"
+VERSION=$(cat version/number)
+REL_DIR="_build/$MIX_ENV/rel/phx_bench_api/releases/$VERSION"
 OUTPUT_ARTIFACT="$REL_DIR/phx_bench_api.tar.gz"
 
-export DATABASE_URL=ecto://phx_prod_user:pass123@meluhha-postgresql/phx_bench_api_prod
 
 cd "$*"
-echo 'Fetch deps, compile, test & release build...'
-mix do deps.get, deps.compile, release --env=prod --verbose
+echo 'Fetch deps, compile & prod release build...'
+mix do deps.get, deps.compile, release
 cp Dockerfile $TARGET_DIR
-cp "_build/dev/rel/phx_bench_api/releases/$VERSION/phx_bench_api.tar.gz" $TARGET_DIR
-echo $TARGET_DIR
+cp $OUTPUT_ARTIFACT $TARGET_DIR
 ls $TARGET_DIR
 echo 'Success!'
